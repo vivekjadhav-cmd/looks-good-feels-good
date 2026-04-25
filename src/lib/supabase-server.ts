517a8683051ada0f36980aae1052d8dba504d6d2
Vivek <vivek.jadhav@gmail.com>
2026-04-25 17:59:25 +0800

    fix type error

--- src/lib/supabase-server.ts
+++ src/lib/supabase-server.ts
@@ -12,7 +12,7 @@ export async function createServerSupabase() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
